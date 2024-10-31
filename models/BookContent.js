1eb48c7a31038c7481764d813216d709d112d35f
luxifearzz <luxifearzz@gmail.com>
2024-10-31 15:36:32 +0700

    Change book content schema

--- models/BookContent.js
+++ models/BookContent.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bookContentSchema = new mongoose.Schema({
     book_id: { type: mongoose.Schema.Types.ObjectId, required: true },
     chapter_number: { type: Number, required: true },
+    title: { type: String, required: true },
     content: { type: String, required: true },
 });
 
